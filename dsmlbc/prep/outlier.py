a868639740d8b4c781b16b5504aaddfa9ed9c67c
Mustafa Vahit Keskin <m.vahitkeskin@gmail.com>
2020-12-17 16:38:20 +0100

    Add files via upload

---
+++ dsmlbc/prep/outlier.py
@@ -0,0 +1,73 @@
+def check_df(dataframe):
+    print("##################### Shape #####################")
+    print(dataframe.shape)
+    print("##################### Types #####################")
+    print(dataframe.dtypes)
+    print("##################### Head #####################")
+    print(dataframe.head(3))
+    print("##################### Tail #####################")
+    print(dataframe.tail(3))
+    print("##################### NA #####################")
+    print(dataframe.isnull().sum())
+    print("##################### Quantiles #####################")
+    print(dataframe.quantile([0.05, 0.10, 0.30, 0.50, 0.75, 0.90, 0.95, 0.99]).T)
+
+
+def outlier_thresholds(dataframe, col_name):
+    quartile1 = dataframe[col_name].quantile(0.10)
+    quartile3 = dataframe[col_name].quantile(0.90)
+    interquantile_range = quartile3 - quartile1
+    up_limit = quartile3 + 1.5 * interquantile_range
+    low_limit = quartile1 - 1.5 * interquantile_range
+    return low_limit, up_limit
+
+
+def check_outlier(dataframe, col_name):
+    low_limit, up_limit = outlier_thresholds(dataframe, col_name)
+    if dataframe[(dataframe[col_name] > up_limit) | (dataframe[col_name] < low_limit)].any(axis=None):
+        print(col_name, "yes")
+    else:
+        print(col_name, "No")
+
+
+def check_outlier_adv(dataframe, num_columns, plot=False):
+    variable_names = []  # aykırı deger sahip değişken isimleri tutulacak.
+    for col in num_columns:
+        low_limit, up_limit = outlier_thresholds(dataframe, col)
+        if dataframe[(dataframe[col] > up_limit) | (dataframe[col] < low_limit)].any(axis=None):
+            number_of_outliers = dataframe[(dataframe[col] > up_limit) | (dataframe[col] < low_limit)].shape[0]
+            print(col, ":", number_of_outliers)
+            variable_names.append(col)
+            if plot:
+                sns.boxplot(x=dataframe[col])
+                plt.show()
+    return variable_names
+
+
+def grab_outliers(dataframe, col_name, index=False):
+    low, up = outlier_thresholds(dataframe, col_name)
+    if dataframe[((dataframe[col_name] < low) | (dataframe[col_name] > up))].shape[0] > 10:
+        print(dataframe[((dataframe[col_name] < low) | (dataframe[col_name] > up))].head())
+    else:
+        print(dataframe[((dataframe[col_name] < low) | (dataframe[col_name] > up))])
+
+    if index:
+        outlier_index = dataframe[((dataframe[col_name] < low) | (dataframe[col_name] > up))].index
+        return outlier_index
+
+
+def remove_outlier(dataframe, col_name):
+    low_limit, up_limit = outlier_thresholds(dataframe, col_name)
+    df_without_outliers = dataframe[~((dataframe[col_name] < low_limit) | (dataframe[col_name] > up_limit))]
+    return df_without_outliers
+
+
+def replace_with_thresholds(dataframe, col_name):
+    low_limit, up_limit = outlier_thresholds(dataframe, col_name)
+    if low_limit > 0:
+        dataframe.loc[(dataframe[col_name] < low_limit), col_name] = low_limit
+        dataframe.loc[(dataframe[col_name] > up_limit), col_name] = up_limit
+    else:
+        dataframe.loc[(dataframe[col_name] > up_limit), col_name] = up_limit
+
+
